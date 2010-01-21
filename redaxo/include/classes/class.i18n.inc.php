900fb381fef78f159f8ba52fe6640eeb22273c9d
Christoph <christoph@webvariants.de>
2010-01-21 00:01:22 +0000

    i18n-Klasse aufgehübscht

--- redaxo/include/classes/class.i18n.inc.php
+++ redaxo/include/classes/class.i18n.inc.php
@@ -9,162 +9,143 @@
 
 class i18n
 {
-  var $locales;
-  var $searchpath;
-  var $locale;
-  var $text;
-	var $text_loaded;
+	protected $locales;
+	protected $searchpath;
+	protected $locale;
+	protected $text;
+	protected $text_loaded;
 
-  /*
-   * Constructor
-   * the locale must of the common form, eg. de_de, en_us or just plain en, de.
-   * the searchpath is where the language files are located
-   */
-  function i18n($locale = "de_de", $searchpath)
-  {
-    $this->searchpath = $searchpath;
-    $this->text = array ();
-    $this->locale = $locale;
-    $this->locales = array ();
-    $this->text_loaded = FALSE;
-  }
+	/*
+	 * Constructor
+	 * the locale must of the common form, eg. de_de, en_us or just plain en, de.
+	 * the searchpath is where the language files are located
+	 */
+	public function __construct($locale = 'de_de', $searchpath)
+	{
+		$this->searchpath  = $searchpath;
+		$this->text        = array();
+		$this->locale      = $locale;
+		$this->locales     = array();
+		$this->text_loaded = false;
+	}
 
-  /*
-   * Lädt alle Übersetzungen der aktuellen Sprache aus dem Sprachpfad und fügt diese dem Katalog hinzu.
-   */
-  function loadTexts()
-  {
-    if($this->appendFile($this->searchpath))
-    {
-  		$this->text_loaded = TRUE;
-    }
-  }
-  
-  /**
-   * Sucht im angegebenden Ordner nach eine Sprachdatei der aktuellen Sprache und fügt diese dem Sprachkatalog an
-   *  
-   * @param $searchPath Pfad in dem die Sprachdatei gesucht werden soll
-   */
-  function appendFile($searchPath)
-  {
-    $filename = $searchPath . DIRECTORY_SEPARATOR . $this->locale . ".lang";
-    if (is_readable($filename))
-    {
-      $handle = fopen($filename, "r");
-      if($handle)
-      {
-        while (!feof($handle))
-        {
-          $buffer = fgets($handle, 4096);
-          if (preg_match("/^(\w*)\s*=\s*(.*)$/", $buffer, $matches))
-          {
-            $this->addMsg($matches[1], trim($matches[2]));
-          }
-        }
-        fclose($handle);
-        return TRUE;
-      }
-    }
-    
-    return FALSE;
-  }
+	/*
+	 * Lädt alle Übersetzungen der aktuellen Sprache aus dem Sprachpfad und fügt diese dem Katalog hinzu.
+	 */
+	public function loadTexts()
+	{
+		if ($this->appendFile($this->searchpath)) {
+			$this->text_loaded = TRUE;
+		}
+	}
 
-  /**
-   * Durchsucht den Sprachkatalog nach einem Schlüssel und gibt die dazugehörige Übersetzung zurück
-   * 
-   * @param $key Zu suchender Schlüssel
-   */
-  function msg($key)
-  {
-  	global $REX;
-  	
-  	/*
-  	// Warum hier umschalten der Sprache!?
-  	if(isset($REX['LOGIN']) && is_object($REX['LOGIN']) && 
-  	   $REX['LOGIN']->getLanguage() != $this->locale)
-  	{
-  		$this->locale = $REX['LOGIN']->getLanguage();
-  		$this->text_loaded = FALSE;
-  	}
-  	*/
-  	
-  	if(!$this->text_loaded)
-  	{
-  	  $this->loadTexts();
-  	}
-  	
-    if ($this->hasMsg($key))
-    {
-      $msg = $this->text[$key];
-    }
-    else
-    {
-      $msg = "[translate:$key]";
-    }
+	/**
+	 * Sucht im angegebenden Ordner nach eine Sprachdatei der aktuellen Sprache und fügt diese dem Sprachkatalog an
+	 *  
+	 * @param string $searchPath  Pfad in dem die Sprachdatei gesucht werden soll
+	 */
+	function appendFile($searchPath)
+	{
+		$filename = $searchPath.'/'.$this->locale.'.lang';
+		
+		if (is_readable($filename)) {
+			$lines = array_map('trim', file($filename));
+			
+			foreach ($lines as $line) {
+				if (preg_match('#^(\w*)\s*=\s*(.*)$#', $buffer, $matches)) {
+					$this->addMsg($matches[1], trim($matches[2]));
+				}
+			}
+			
+			return true;
+		}
 
-    $patterns = array ();
-    $replacements = array ();
+		return false;
+	}
 
-    $args = func_get_args();
-    for($i = 1; $i < func_num_args(); $i++)
-    {
-      // zero indexed
-      $patterns[] = '/\{'. ($i-1) .'\}/';
-      $replacements[] = $args[$i];
-    }
+	/**
+	 * Durchsucht den Sprachkatalog nach einem Schlüssel und gibt die dazugehörige Übersetzung zurück
+	 * 
+	 * @param string $key  zu suchender Schlüssel
+	 */
+	function msg($key)
+	{
+		global $REX;
 
-    return preg_replace($patterns, $replacements, $msg);
-  }
+		/*
+		// Warum hier umschalten der Sprache!?
+		if (isset($REX['LOGIN']) && is_object($REX['LOGIN']) && $REX['LOGIN']->getLanguage() != $this->locale) {
+			$this->locale = $REX['LOGIN']->getLanguage();
+			$this->text_loaded = FALSE;
+		}
+		*/
 
-  /**
-   * Fügt dem Sprachkatalog unter dem gegebenen Schlüssel eine neue Übersetzung hinzu 
-   *  
-   * @param $key Schlüssel unter dem die Übersetzung abgelegt wird
-   * @param $msg Übersetzter Text
-   */
-  function addMsg($key, $msg)
-  {
-    $this->text[$key] = $msg;
-  }
+		if (!$this->text_loaded) {
+			$this->loadTexts();
+		}
 
-  /**
-   * Prüft ob der Sprachkatalog zu dem gegebenen Schlüssel eine Übersetzung beinhaltet
-   * 
-   * @param $key Zu suchender Schlüssel
-   * @return boolean TRUE Wenn der Schlüssel gefunden wurde, sonst FALSE
-   */
-  function hasMsg($key)
-  {
-  	return isset ($this->text[$key]);
-  }
+		if ($this->hasMsg($key)) {
+			$msg = $this->text[$key];
+		}
+		else {
+			$msg = '[translate:'.$key.']';
+		}
 
-  /**
-   * Durchsucht den Searchpath nach allen verfügbaren Sprachdateien und gibt diese zurück
-   * 
-   * @param $searchpath Zu duruchsuchender Ordner
-   * @return array Array von gefundenen Sprachen (locales)
-   */
-  function getLocales($searchpath)
-  {
-    if (empty ($this->locales) && is_readable($searchpath))
-    {
-      $this->locales = array ();
+		$patterns     = array();
+		$replacements = array();
 
-      $handle = opendir($searchpath);
-      while ($file = readdir($handle))
-      {
-        if ($file != "." && $file != "..")
-        {
-          if (preg_match("/^(\w+)\.lang$/", $file, $matches))
-          {
-            $this->locales[] = $matches[1];
-          }
-        }
-      }
-      closedir($handle);
+		$argv = func_get_args();
+		$argc = count($argv);
+		
+		// Wir überspringen $key -> $i = 1
+		for ($i = 1; $i < $argc; ++$i) {
+			$patterns[]     = '/\{'.($i-1).'\}/';
+			$replacements[] = $args[$i];
+		}
 
-    }
+		return preg_replace($patterns, $replacements, $msg);
+	}
 
-    return $this->locales;
-  }
+	/**
+	 * Fügt dem Sprachkatalog unter dem gegebenen Schlüssel eine neue Übersetzung hinzu 
+	 *  
+	 * @param string $key  Schlüssel unter dem die Übersetzung abgelegt wird
+	 * @param string $msg  übersetzter Text
+	 */
+	function addMsg($key, $msg) {
+		$this->text[$key] = $msg;
+	}
+
+	/**
+	 * Prüft ob der Sprachkatalog zu dem gegebenen Schlüssel eine Übersetzung beinhaltet
+	 * 
+	 * @param  string $key zu suchender Schlüssel
+	 * @return boolean     true wenn der Schlüssel gefunden wurde, sonst false
+	 */
+	function hasMsg($key)
+	{
+		return isset($this->text[$key]);
+	}
+
+	/**
+	 * Durchsucht den Searchpath nach allen verfügbaren Sprachdateien und gibt diese zurück
+	 * 
+	 * @param  string $searchpath  zu duruchsuchender Ordner
+	 * @return array               Array von gefundenen Sprachen (locales)
+	 */
+	function getLocales($searchpath)
+	{
+		if (empty($this->locales) && is_readable($searchpath)) {
+			$this->locales = array();
+
+			$files = glob($searchpath.'/*.lang');
+			
+			foreach ($files as $filename) {
+				// von 'C:\foo\bar.lang' nur 'bar' speichern
+				$this->locales[] = substr(basename($filename), 0, -5);
+			}
+		}
+
+		return $this->locales;
+	}
 }
