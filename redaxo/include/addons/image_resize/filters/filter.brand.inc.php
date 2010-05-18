9904647f0a878eaed31782223eef40dd0b15aaa8
Christoph <christoph@webvariants.de>
2010-05-18 14:37:25 +0200

    Filter-Code aufgehübscht

--- redaxo/include/addons/image_resize/filters/filter.brand.inc.php
+++ redaxo/include/addons/image_resize/filters/filter.brand.inc.php
@@ -5,56 +5,62 @@
  * Der Filter sucht im Verzeichnis addons/image_resize/media/
  * nach einem Bild mit dem Dateinamen "brand.*" und verwendet den 1. Treffer
  */
-function image_resize_brand(& $src_im)
+function image_resize_brand(&$src_im)
 {
-  global $REX;
+	global $REX;
 
-  $files = glob($REX['INCLUDE_PATH'] . '/addons/image_resize/media/brand.*');
-  $brandImage = $files[0];
-  $brand = new rex_thumbnail($brandImage);
+	$files = glob($REX['INCLUDE_PATH'].'/addons/image_resize/media/brand.*');
+	
+	if (empty($files)) {
+		return;
+	}
+	
+	$brandImage = $files[0];
+	$brand      = new Thumbnail($brandImage);
 
-  // -------------------------------------- CONFIG
-  
-  // Abstand vom Rand
-  $paddX = -10;
-  $paddY = -10;
-  // horizontale ausrichtung: left/center/right
-  $hpos = 'right'; 
-  // vertikale ausrichtung:   top/center/bottom
-  $vpos = 'bottom';
-  
-  // -------------------------------------- /CONFIG
-  
-  switch($hpos)
-  {
-    case 'left':
-      $dstX = 0;
-      break;
-    case 'center':
-      $dstX = (int)((imagesx($src_im) - $brand->getImageWidth()) / 2);
-      break;
-    case 'right':
-      $dstX = imagesx($src_im) - $brand->getImageWidth();
-      break;
-    default: trigger_error('Unexpected value for "hpos"!', E_USER_ERROR);
-  }
-  
-  switch($vpos)
-  {
-    case 'top':
-      $dstY = 0;
-      break;
-    case 'center':
-      $dstY = (int)((imagesy($src_im) - $brand->getImageHeight()) / 2);
-      break;
-    case 'bottom':
-      $dstY = imagesy($src_im) - $brand->getImageHeight();
-      break;
-    default: trigger_error('Unexpected value for "vpos"!', E_USER_ERROR);
-  }
-  
-  imagealphablending($src_im, true);
-  imagecopy($src_im, $brand->getImage(), $dstX + $paddX, $dstY + $paddY, 0, 0, $brand->getImageWidth(), $brand->getImageHeight());
+	// Abstand vom Rand
+	$paddX = -10;
+	$paddY = -10;
 
-  $brand->destroyImage();
+	$hpos = 'right';  // horizontale Ausrichtung: left/center/right
+	$vpos = 'bottom'; // vertikale Ausrichtung: top/center/bottom
+
+	switch ($hpos) {
+		case 'left':
+			$dstX = 0;
+			break;
+
+		case 'center':
+			$dstX = (int) ((imagesx($src_im) - $brand->getImageWidth()) / 2);
+			break;
+
+		case 'right':
+			$dstX = imagesx($src_im) - $brand->getImageWidth();
+			break;
+
+		default:
+			trigger_error('Unexpected value for "hpos"!', E_USER_ERROR);
+	}
+
+	switch ($vpos) {
+		case 'top':
+			$dstY = 0;
+			break;
+
+		case 'center':
+			$dstY = (int) ((imagesy($src_im) - $brand->getImageHeight()) / 2);
+			break;
+
+		case 'bottom':
+			$dstY = imagesy($src_im) - $brand->getImageHeight();
+			break;
+
+		default:
+			trigger_error('Unexpected value for "vpos"!', E_USER_ERROR);
+	}
+
+	imagealphablending($src_im, true);
+	imagecopy($src_im, $brand->getImage(), $dstX + $paddX, $dstY + $paddY, 0, 0, $brand->getImageWidth(), $brand->getImageHeight());
+
+	$brand->destroyImage();
 }
