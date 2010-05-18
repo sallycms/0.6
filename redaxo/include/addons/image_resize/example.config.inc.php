5f1bc20467b9d9f71285917416d8f202c23ab0d7
Christoph <christoph@webvariants.de>
2010-05-18 18:28:44 +0200

    Dummy-config.inc.php für ImageResize ergänzt

---
+++ redaxo/include/addons/image_resize/example.config.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+// --- DYN
+$REX['ADDON']['image_resize']['max_cachefiles']  = 5;
+$REX['ADDON']['image_resize']['max_filters']     = 5;
+$REX['ADDON']['image_resize']['max_resizekb']    = 1000;
+$REX['ADDON']['image_resize']['max_resizepixel'] = 2000;
+$REX['ADDON']['image_resize']['jpg_quality']     = 75;
+// --- /DYN
