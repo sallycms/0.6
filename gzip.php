1c8d24f1ab22fa7bd98a778ab789fa0f3673407d
Christoph <christoph@webvariants.de>
2010-05-19 18:56:13 +0200

    gzip.php verbessert

--- gzip.php
+++ gzip.php
@@ -1,4 +1,8 @@
 <?php
+
+error_reporting(0);
+ini_set('display_errors', 'off');
+
 $file = realpath(@$_GET['file']);
 if ($file === false) die;
 
@@ -14,9 +18,47 @@ $mimetypes = array(
 
 if (!file_exists($file)) die;
 if (substr($file,-3) != '.js' && substr($file,-4) != '.css') die;
-if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) ob_start('ob_gzhandler');
+
+// E-Tag-Behandlung
+
+$etag = !empty($_SERVER['HTTP_IF_NONE_MATCH']) ? $_SERVER['HTTP_IF_NONE_MATCH'] : '';
+$md5  = substr(md5_file($file), 0, 10);
+
+if ($etag == '"'.$md5.'"') { // ETag steht in "..."
+	header('HTTP/1.1 304 Not Modified');
+	exit();
+}
+
+// Last-Modified-Since-Behandlung
+
+$last  = !empty($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? $_SERVER['HTTP_IF_MODIFIED_SINCE'] : -1;
+$mtime = filemtime($file);
+
+if ($last > 0 && $last == $mtime) {
+	header('HTTP/1.1 304 Not Modified');
+	exit();
+}
+
+// gzip starten
+
+if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) {
+	ob_start('ob_gzhandler');
+}
+
+// Content-Type senden
 
 $extension = strtolower(substr($file, strrpos($file, '.')));
 if (isset($mimetypes[$extension])) header('Content-Type: '.$mimetypes[$extension]);
+
+// Headers
+
+header('Last-Modified: '.date('r', $mtime));
+header('ETag: "'.$md5.'"');
+
+if (!empty($_GET['forever'])) {
+	header('Expires: Thu, 15 Apr '.(date('Y')+2).' 20:00:00 GMT');
+}
+
+// Content senden
 
 readfile($file);
