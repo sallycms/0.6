99d7d6da4af43cd438be0ec19f5532f498632bf7
Christoph <christoph@webvariants.de>
2010-05-24 20:26:53 +0200

    sly_Registry_Persistent ausimplementiert

--- redaxo/include/lib/sly/Registry/Persistent.php
+++ redaxo/include/lib/sly/Registry/Persistent.php
@@ -12,8 +12,15 @@
 class sly_Registry_Persistent implements sly_Registry_Registry {
 
 	private static $instance;
+	private $store;
+	private $pdo;
+	private $prefix;
 	
-	private function __construct() {}
+	private function __construct() {
+		$this->store  = new sly_Util_Array();
+		$this->pdo    = sly_DB_PDO_Persistence::getInstance();
+		$this->prefix = sly_Core::config()->get('TABLE_PREFIX');
+	}
 	
 	public static function getInstance() {
 		if (empty(self::$instance)) self::$instance = new self();
@@ -21,19 +28,48 @@ class sly_Registry_Persistent implements sly_Registry_Registry {
 	}
 	
 	public function set($key, $value) {
+		$qry = 'REPLACE INTO '.$this->prefix.'registry (`key`, `value`) VALUES (?,?)';
+		$this->pdo->query($qry, array($key, serialize($value)));
 		
+		return $this->store->set($key, $value);
 	}
 	
 	public function get($key) {
+		if (!$this->store->has($key)) {
+			$value = $this->getValue($key);
+			
+			if ($value !== false) {
+				$value = unserialize($value);
+				$this->store->set($key, $value);
+				return $value; // schneller als nochmal get() aufzurufen
+			}
+			
+			// fallthrough -> Fehlerbehandlung durch sly_Util_Array
+		}
 		
+		return $this->store->get($key);
 	}
 	
 	public function has($key) {
+		if ($this->store->has($key)) return true;
 		
+		$value = $this->getValue($key);
+		
+		if ($value !== false) {
+			$value = unserialize($value);
+			$this->store->set($key, $value);
+			return true;
+		}
+		
+		return false;
 	}
 	
 	public function remove($key) {
-		
+		$this->pdo->query('DELETE FROM '.$this->prefix.'registry WHERE `key` = ?', array($key));
+		return $this->store->remove($key);
 	}
 	
+	protected static function getValue($key) {
+		return $this->pdo->magicFetch('`value`', 'registry', '`key` = ?', array($key));
+	}
 }
