45f3137b478a3f84a849c147f4a9ff7a2ee72c7c
Christoph <christoph@webvariants.de>
2010-06-08 23:35:18 +0200

    sly_Log aufgebohrt
    --HG--
    extra : rebase_source : ffc8f1c36c65f2d839e4fbf191acab3c91aed1a8

--- redaxo/include/lib/sly/Log.php
+++ redaxo/include/lib/sly/Log.php
@@ -28,6 +28,7 @@ class sly_Log
 	
 	const FORMAT_SIMPLE   = '[%date% %time%] %typename%: %message%';
 	const FORMAT_EXTENDED = '[%date% %time%] %typename%: %message% (IP: %ip%, Hostname: %hostname%)';
+	const FORMAT_CALLER   = '[%date% %time%] %typename%: %message% (%caller%, %called%)';
 	
 	private function __construct($filename)
 	{
@@ -49,57 +50,44 @@ class sly_Log
 		return new self($dir.'/'.$name.'.log');
 	}
 	
-	public static function setFormat($format)
+	public function setFormat($format)
 	{
 		$this->format = trim($format);
 	}
 	
-	public function info($text)    { $this->log(self::LEVEL_INFO, $text);    }
-	public function warning($text) { $this->log(self::LEVEL_WARNING, $text); }
-	public function error($text)   { $this->log(self::LEVEL_ERROR, $text);   }
+	public function info($text, $depth = 1)    { return $this->log(self::LEVEL_INFO,    $text, $depth + 1); }
+	public function warning($text, $depth = 1) { return $this->log(self::LEVEL_WARNING, $text, $depth + 1); }
+	public function error($text, $depth = 1)   { return $this->log(self::LEVEL_ERROR,   $text, $depth + 1); }
 	
-	public function log($level, $message)
+	public function log($level, $message, $depth = 1)
 	{
 		if ($level != self::LEVEL_INFO && $level != self::LEVEL_ERROR && $level != self::LEVEL_WARNING) {
 			throw new sly_Exception('Unbekannter Nachrichtentyp: '.$level);
 		}
 		
-		$line = $this->format;
-		$line = str_replace('%message%', $message, $line);
-		$line = str_replace('%ip%', $_SERVER['REMOTE_ADDR'], $line);
-		$line = str_replace('%date%', strftime('%Y-%m-%d'), $line);
-		$line = str_replace('%time%', strftime('%H:%M:%S'), $line);
-		
-		if (strpos($line, '%hostname%') !== false) {
-			$host = gethostbyaddr($_SERVER['REMOTE_ADDR']);
-			$line = str_replace('%hostname%', $host, $line);
-		}
-		
 		switch ($level) {
 			case self::LEVEL_ERROR:
-				$line = str_replace('%typename%', 'Error', $line);
+				$line = $this->replacePlaceholders($this->format, 'Error', $depth);
 				break;
 				
 			case self::LEVEL_WARNING:
-				$line = str_replace('%typename%', 'Warning', $line);
+				$line = $this->replacePlaceholders($this->format, 'Warning', $depth);
 				break;
 				
 			case self::LEVEL_INFO:
-				$line = str_replace('%typename%', 'Info', $line);
+				$line = $this->replacePlaceholders($this->format, 'Info', $depth);
 				break;
 		}
 		
+		$line = str_replace('%message%', $message, $line);
 		$line = trim($line);
+		
 		return file_put_contents($this->filename, $line."\n", FILE_APPEND) > 0;
 	}
 	
-	public function dump($name, $value, $force_style = null)
+	public function dump($name, $value, $force_style = null, $depth = 1)
 	{
-		$line = $this->format;
-		$line = str_replace('%ip%', $_SERVER['REMOTE_ADDR'], $line);
-		$line = str_replace('%date%', strftime('%Y-%m-%d'), $line);
-		$line = str_replace('%time%', strftime('%H:%M:%S'), $line);
-		$line = str_replace('%typename%', 'Dump', $line);
+		$line = $this->replacePlaceholders($this->format, 'Dump', $depth);
 		
 		if ($force_style) {
 			$value = $force_style($value);
@@ -140,6 +128,93 @@ class sly_Log
 		
 		$line = str_replace('%message%', '$'.$name.' = '.$value, $line);
 		$line = trim($line);
+
 		return file_put_contents($this->filename, $line."\n", FILE_APPEND) > 0;
+	}
+	
+	protected function replacePlaceholders($line, $typename, $depth)
+	{
+		$ip   = empty($_SERVER['REMOTE_ADDR']) ? '<not set>' : $_SERVER['REMOTE_ADDR'];
+		$line = str_replace('%ip%', $ip, $line);
+		$line = str_replace('%date%', strftime('%Y-%m-%d'), $line);
+		$line = str_replace('%time%', strftime('%H:%M:%S'), $line);
+		$line = str_replace('%typename%', $typename, $line);
+		
+		if (strpos($line, '%hostname%') !== false) {
+			$host = empty($_SERVER['REMOTE_ADDR']) ? 'N/A' : gethostbyaddr($_SERVER['REMOTE_ADDR']);
+			$line = str_replace('%hostname%', $host, $line);
+		}
+		
+		if (strpos($line, '%caller%') !== false || strpos($line, '%called%') !== false) {
+			$caller = $this->getCaller($depth + 1);
+			$file   = $caller['line'] == 0 ? '[PHPcore]' : basename($caller['file']).':'.$caller['line'];
+			$line   = str_replace('%called%', $file, $line);
+			
+			if ($caller['caller'] != '[PHPcore]') {
+				$line = str_replace('%caller%', $caller['caller'].'()', $line);
+			}
+			else {
+				$line = str_replace('%caller%', $caller['caller'], $line);
+			}
+		}
+		
+		return $line;
+	}
+	
+	protected function getCaller($depth)
+	{
+		$trace = array_slice(debug_backtrace(), $depth);
+
+		// Wurde getCaller() nicht innerhalb einer Funktion aufgerufen?
+
+		if (empty($trace)) {
+			return array();
+		}
+
+		// OOP-Aufruf?
+
+		if (isset($trace[0]['class'])) {
+			$trace[0]['function'] = $trace[0]['class'].$trace[0]['type'].$trace[0]['function'];
+		}
+
+		if (isset($trace[1]['class'])) {
+			$trace[1]['function'] = $trace[1]['class'].$trace[1]['type'].$trace[1]['function'];
+		}
+
+		// Im ersten Element steht, von wo aus die Funktion aufgerufen
+		// wurde.
+
+		$calledBy = array(
+			'file'     => isset($trace[0]['file']) ? $trace[0]['file'] : '[PHPcore]',
+			'line'     => isset($trace[0]['line']) ? $trace[0]['line'] : 0,
+			'function' => $trace[0]['function'], // der Name der Fkt., die getCaller() aufgerufen hat
+			'caller'   => '[PHPcore]',
+			'byphp'    => false
+		);
+
+		// Falls wir noch ein Element im Array haben, dann wissen wir
+		// sogar, wie die Funktion heißt, die die Funktion aufgerufen
+		// hat.
+
+		if (count($trace) >= 2) {
+			$calledBy['caller'] = $trace[1]['function'];
+		}
+
+		// Falls die Funktion durch PHP aufgerufen wurde (array_walk o.ä.)
+		// holen wir nicht den Aufruf durch array_walk (da wir das ja nicht
+		// wissen können), sondern den Aufruf von array_walk.
+
+		// Der Aufruf könnte auch von z.B. register_shutdown_function()
+		// kommen. In dem Fall haben wir hingegen keine Angabe, wo
+		// genau im Code register_shutdown_function aufgerufen
+		// wurde.
+
+		if ($calledBy['file'] == '[PHPcore]' && count($trace) >= 2) {
+			$calledBy['file']  = $trace[1]['file'];
+			$calledBy['line']  = $trace[1]['line'];
+			$calledBy['byphp'] = true;
+		}
+
+		return $calledBy;
 	}
 }
