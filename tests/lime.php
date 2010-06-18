f48b55816835863edf66304c121f2134e09ccfb9
Christoph <christoph@webvariants.de>
2010-06-18 18:12:25 +0200

    Lime-Tests können im Browser aufgerufen werden.

--- tests/lime.php
+++ tests/lime.php
@@ -7,6 +7,13 @@ $registration = new lime_registration();
 $registration->extension = '.lime.php';
 $registration->register_dir(dirname(__FILE__).'/tests/lime');
 
+if (isset($_SERVER['REMOTE_ADDR'])) {
+	header('Content-Type: text/html; charset=UTF-8');
+	print '<pre>';
+}
+
+$lime = new lime_test();
+
 foreach ($registration->files as $filename) {
 	include $filename;
 }
