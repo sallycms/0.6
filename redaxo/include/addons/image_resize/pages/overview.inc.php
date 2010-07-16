d4656637e66c35e96e056e3ff1312641ad4fad56
Christoph <christoph@webvariants.de>
2010-07-16 04:32:30 +0200

    Anzeige der Versionsnummer von ImageResize korrigiert

--- redaxo/include/addons/image_resize/pages/overview.inc.php
+++ redaxo/include/addons/image_resize/pages/overview.inc.php
@@ -1,5 +1,7 @@
-<div class="rex-addon-output">
-  <h2 class="rex-hl2">Image Resize Addon (Version <?= sly_html($REX['ADDON']['version']['image_resize']) ?>)</h2>
+<?
+$version = sly_Service_Factory::getService('AddOn')->getVersion('image_resize');
+?><div class="rex-addon-output">
+  <h2 class="rex-hl2">Image Resize Addon (Version <?= sly_html($version) ?>)</h2>
   <div class="rex-addon-content">
     <?php include dirname(__FILE__).'/../help.inc.php'; ?>
   </div>
