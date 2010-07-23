e75a2c42ab0f157e86f26ecdd933e5b5139910da
Christoph <christoph@webvariants.de>
2010-07-23 03:08:30 +0200

    Updated unit tests to comply with new sly_Event_Dispatcher behaviour.

--- tests/tests/lime/Event/dispatcher.lime.php
+++ tests/tests/lime/Event/dispatcher.lime.php
@@ -115,17 +115,9 @@ $result = runEventDispatcherTest('a,c,b', 'notifyUntil', 'foo');
 $lime->is($result, true, 'notifyUntil() returns true because listenerC returned true');
 $lime->is($___STATE, array('a', 'c'), 'notifyUntil() really stopped at listenerC');
 
-// notifyUntil() ===============================================================
 
-$result = runEventDispatcherTest('d,a,c,b', 'notifyUntil', 'foo');
-$lime->is($result, 'hello world', 'notifyUntil() returns the return value of listenerD');
-$lime->is($___STATE, array('d'), 'notifyUntil() really stopped at listenerD');
 
-// notifyUntil() ===============================================================
 
-$result = runEventDispatcherTest('a,e', 'notifyUntil', 'return_me');
-$lime->is($result, 'return_me', 'notifyUntil() correctly passes the subject to the listeners');
-$lime->is($___STATE, array('a', 'e'), 'notifyUntil() really stopped at listenerE');
 
 // filter() ====================================================================
 
