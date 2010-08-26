0ffbfb362c4a6f6d0d278f4e262c345fcb382fe5
Stephan <stephan@webvariants.de>
2010-08-26 14:25:12 +0200

    Linkbutton unter 0.2

--- redaxo/include/lib/sly/Form/LinkButton.php
+++ redaxo/include/lib/sly/Form/LinkButton.php
@@ -9,32 +9,32 @@
  * http://de.wikipedia.org/wiki/MIT-Lizenz
  */
 
-class sly_Form_LinkButton extends sly_Form_Element_Base implements sly_Form_IElement
+class sly_Form_LinkButton extends sly_Form_ElementBase implements sly_Form_IElement
 {
 	protected $javascriptID;
-	
+
 	public function __construct($name, $label, $value, $javascriptID, $id = null, $allowedAttributes = null)
 	{
 		if ($allowedAttributes === null) {
 			$allowedAttributes = array('value', 'name', 'id', 'disabled', 'class', 'maxlength', 'readonly', 'style');
 		}
-		
+
 		parent::__construct($name, $label, $value, $id, $allowedAttributes);
 		$this->setAttribute('class', 'rex-form-text');
 		$this->javascriptID = $javascriptID;
 	}
-	
+
 	public function render()
 	{
 		// Prüfen, ob das Formular bereits abgeschickt und noch einmal angezeigt
 		// werden soll. Falls ja, übernehmen wir den Wert aus den POST-Daten.
-		
+
 		$name = $this->attributes['name'];
-		
+
 		if (isset($_POST[$name]) && strlen($_POST[$name]) > 0) {
 			$this->attributes['value'] = sly_post($name, 'int', 0);
 		}
-		
+
 		return $this->renderFilename('form/linkbutton.phtml');
 	}
 }
