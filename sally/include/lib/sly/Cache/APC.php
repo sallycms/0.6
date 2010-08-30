93b9014109af0cbf9ea7eda8dc507cd182ee70f1
Christoph <christoph@webvariants.de>
2010-08-31 01:01:35 +0200

    Support APC 1.3.4's apc_exists() method (optionally)

--- sally/include/lib/sly/Cache/APC.php
+++ sally/include/lib/sly/Cache/APC.php
@@ -12,12 +12,22 @@
  * @ingroup cache
  */
 class sly_Cache_APC extends sly_Cache_Abstract {
+	private static $hasExistsMethod = null;
+
 	public static function getMaxKeyLength() {
 		return 200; // unbekannt -> Schätzwert
 	}
 
 	public static function hasLocking() {
-		return false;
+		return true;
+	}
+
+	private static function hasExistsMethod() {
+		if (self::$hasExistsMethod === null) {
+			self::$hasExistsMethod = function_exists('apc_exists');
+		}
+
+		return self::$hasExistsMethod;
 	}
 
 	public static function isAvailable() {
@@ -25,17 +35,42 @@ class sly_Cache_APC extends sly_Cache_Abstract {
 		return function_exists('apc_store') && apc_store('test', 1, 1);
 	}
 
-	protected function _getRaw($key) { return apc_fetch($key); }
-	protected function _get($key)    { return apc_fetch($key); }
+	protected function _getRaw($key) {
+		return apc_fetch($key);
+	}
 
-	protected function _setRaw($key, $value, $expiration) { return apc_store($key, $value, $expiration); }
-	protected function _set($key, $value, $expiration)    { return apc_store($key, $value, $expiration); }
+	protected function _get($key) {
+		$value = apc_fetch($key);
+		return self::hasExistsMethod() ? $value : unserialize($value);
+	}
 
-	protected function _delete($key) { return apc_delete($key);          }
-	protected function _isset($key)  { return apc_fetch($key) !== false; }
+	protected function _setRaw($key, $value, $expiration) {
+		return apc_store($key, $value, $expiration);
+	}
+
+	protected function _set($key, $value, $expiration) {
+		if (!self::hasExistsMethod()) $value = serialize($value);
+		return apc_store($key, $value, $expiration);
+	}
+
+	protected function _delete($key) {
+		return apc_delete($key);
+	}
+
+	protected function _isset($key) {
+		if (self::hasExistsMethod()) return apc_exists($key);
+		return apc_fetch($key) !== false;
+	}
 
 	protected function _increment($key) {
-		$value = apc_fetch($key);
-		return apc_store($key, $value + 1) !== false;
+		return apc_inc($key) !== false;
+	}
+
+	protected function _lock($key) {
+		return apc_add($key, 1);
+	}
+
+	protected function _unlock($key) {
+		return apc_delete($key);
 	}
 }
