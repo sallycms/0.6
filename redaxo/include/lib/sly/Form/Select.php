56f59391a15fc20bb5d5146148d3c4ff9dd951e5
Christoph <christoph@webvariants.de>
2010-09-09 21:29:05 +0200

    fixes notices when using sly_Form_Select directly

--- redaxo/include/lib/sly/Form/Select.php
+++ redaxo/include/lib/sly/Form/Select.php
@@ -12,40 +12,40 @@
 class sly_Form_Select extends sly_Form_Select_Base implements sly_Form_IElement
 {
 	protected $formElement;
-	
+
 	const STYLE_DROPDOWN = 0;
 	const STYLE_RADIO    = 1;
 	const STYLE_CHECKBOX = 2;
-	
+
 	public function __construct($name, $label, $value, $values, $id = null)
 	{
 		$allowed = array('value', 'name', 'id', 'disabled', 'class', 'style', 'size', 'multiple', 'onselect', 'onchange');
 		parent::__construct($name, $label, $value, $values, $id, $allowed);
 		$this->setStyle(self::STYLE_DROPDOWN);
 	}
-	
+
 	public function setStyle($newStyle)
 	{
 		$name   = $this->attributes['name'];
-		$name   = $this->label;
+		$label  = $this->label;
 		$value  = $this->attributes['value'];
 		$values = $this->values;
 		$id     = $this->attributes['id'];
-		
-		switch ($this->style) {
+
+		switch ($newStyle) {
 			case self::STYLE_CHECKBOX:
 				$this->formElement = new sly_Form_Select_Checkbox($name, $label, $value, $values, $id);
 				break;
-				
+
 			case self::STYLE_RADIO:
 				$this->formElement = new sly_Form_Select_Radio($name, $label, $value, $values, $id);
 				break;
-			
+
 			default:
 				$this->formElement = new sly_Form_Select_DropDown($name, $label, $value, $values, $id);
 				break;
 		}
-		
+
 		return $this->formElement;
 	}
 
