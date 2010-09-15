4fd64169b7a94124487fde882133cca6ca66504a
Dave <dave@webvariants.de>
2010-09-15 16:38:31 +0200

    send 404 for error image

--- redaxo/include/addons/image_resize/classes/class.thumbnail.inc.php
+++ redaxo/include/addons/image_resize/classes/class.thumbnail.inc.php
@@ -127,7 +127,7 @@ class Thumbnail
 	{
 		$service = sly_Service_Factory::getService('AddOn');
 		$folder  = $service->publicFolder('image_resize');
-		self::sendImage($folder.'/'.self::ERRORFILE);
+		self::sendImage($folder.'/'.self::ERRORFILE, true);
 	}
 
 	/**
@@ -593,20 +593,25 @@ class Thumbnail
 	 * @param timestamp $lastModified wann wurde das Bild zuletzt modifiziert?
 	 * @return void
 	 */
-	private static function sendImage($fileName)
+	private static function sendImage($fileName, $error = false)
 	{
 		while (ob_get_level()) ob_end_clean();
 
 		$etag = md5($fileName.filectime($fileName));
 
-		if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && $_SERVER['HTTP_IF_NONE_MATCH'] == $etag) {
-			header('HTTP/1.0 304 Not Modified');
-			exit();
-		}
-		if (!is_readable($fileName)) {
-			trigger_error('The Image "'.$fileName.'" cannot be read.', E_USER_WARNING);
+		if ($error) {
 			header('HTTP/1.0 404 Not Found');
-			exit();
+		}
+		else {
+			if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && $_SERVER['HTTP_IF_NONE_MATCH'] == $etag) {
+				header('HTTP/1.0 304 Not Modified');
+				exit();
+			}
+			if (!is_readable($fileName)) {
+				trigger_error('The Image "'.$fileName.'" cannot be read.', E_USER_WARNING);
+				header('HTTP/1.0 404 Not Found');
+				exit();
+			}
 		}
 
 		header('Content-Type: image/'.self::getFileExtensionStatic($fileName));
