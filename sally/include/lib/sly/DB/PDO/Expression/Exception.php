fac9b6450a2bc643f7b7ed8f93ea3e2fbde78e10
Christoph <christoph@webvariants.de>
2011-02-03 00:39:59 +0100

    added missing exception class

---
+++ sally/include/lib/sly/DB/PDO/Expression/Exception.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Copyright (c) 2011, webvariants GbR, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * @ingroup database
+ */
+class sly_DB_PDO_Expression_Exception extends sly_DB_PDO_Exception { }
