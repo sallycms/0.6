9985d0d50b38f92b2750db535f5011048d3eee59
Christoph <christoph@webvariants.de>
2011-04-28 01:41:58 +0200

    finished moveCategory and fixed flushing the cache (not very efficient, but it works)

--- sally/include/functions/function_rex_content.inc.php
+++ sally/include/functions/function_rex_content.inc.php
@@ -505,14 +505,10 @@ function rex_moveArticle($id, $target) {
 
 		// re-number old category
 		$sql->query('UPDATE '.$pre.'article SET prior = prior - 1 WHERE re_id = '.$source.' AND startpage = 0 AND clang = '.$clang.' AND prior > '.$from['prior']);
-
-		// update cache
-		$cache->delete('sly.article', $id.'_'.$clang);
-		$cache->delete('sly.article.list', $source.'_'.$clang.'_0');
-		$cache->delete('sly.article.list', $source.'_'.$clang.'_1');
-		$cache->delete('sly.article.list', $target.'_'.$clang.'_0');
-		$cache->delete('sly.article.list', $target.'_'.$clang.'_1');
 	}
+
+	// update cache (very generously since many articles have changed (-> been moved))
+	$cache->flush('sly.article', true);
 
 	return true;
 }
@@ -525,100 +521,93 @@ function rex_moveArticle($id, $target) {
  * @return boolean        true bei Erfolg, sonst false
  */
 function rex_moveCategory($from_cat, $to_cat) {
-	$from_cat  = (int) $from_cat;
-	$to_cat    = (int) $to_cat;
-	$prefix    = sly_Core::config()->get('DATABASE/TABLE_PREFIX');
+	$from_cat = (int) $from_cat;
+	$to_cat   = (int) $to_cat;
+	$prefix   = sly_Core::config()->get('DATABASE/TABLE_PREFIX');
+	$sql      = sly_DB_Persistence::getInstance();
+	$cache    = sly_Core::cache();
+	$login    = sly_Util_User::getCurrentUser()->getLogin();
 
 	if ($from_cat == $to_cat) {
-		// kann nicht in gleiche Kategroie kopiert werden
 		return false;
 	}
-	else {
-		// Kategorien vorhanden?
-		// Ist die Zielkategorie im Pfad der Quellkategorie?
 
-		$from_data = rex_sql::fetch('path, re_id', 'article', 'startpage = 1 AND id = '.$from_cat.' AND clang = 1');
-		$to_data   = $to_cat == 0 ? false : rex_sql::fetch('path, re_id', 'article', 'startpage = 1 and id = '.$to_cat.' AND clang = 1');
+	// Kategorien vorhanden?
 
-		if (!$from_data || (!$to_data && $to_cat != 0)) {
-			// eine der Kategorien existiert nicht
+	$from_data = $sql->magicFetch('article', 'path, re_id', array('startpage' => 1, 'id' => $from_cat, 'clang' => 1));
+	$to_data   = $to_cat == 0 ? false : $sql->magicFetch('article', 'path, re_id', array('startpage' => 1, 'id' => $to_cat, 'clang' => 1));
+
+	if (!$from_data || (!$to_data && $to_cat !== 0)) {
+		// eine der Kategorien existiert nicht
+		return false;
+	}
+
+	$oldParent = $from_data['re_id'];
+
+	// Ist die Zielkategorie im Pfad der Quellkategorie?
+
+	if ($to_cat > 0) {
+		$tcats = explode('|', $to_data['path']);
+
+		if (in_array($from_cat, $tcats)) {
+			// Zielkategorie ist in Quellkategorie -> nicht verschiebbar
 			return false;
 		}
-		else {
-			if ($to_cat > 0) {
-				$tcats = explode('|', $to_data['path']);
-
-				if (in_array($from_cat, $tcats)) {
-					// Zielkategorie ist in Quellkategorie -> nicht verschiebbar
-					return false;
-				}
-			}
-
-			// folgende cats regenerate
-
-			$RC[$from_data['re_id']] = 1;
-			$RC[$from_cat]           = 1;
-			$RC[$to_cat]             = 1;
-
-			if ($to_cat > 0) {
-				$to_path  = $to_data['path'].$to_cat.'|';
-				$to_re_id = $to_data['re_id'];
-			}
-			else {
-				$to_path  = '|';
-				$to_re_id = 0;
-			}
-
-			$from_path = $from_data['path'].$from_cat.'|';
-
-			$up   = new rex_sql();
-			$cats = $up->getArrayEx('SELECT id, re_id, path FROM '.$prefix.'article WHERE path LIKE "'.$from_path.'%" AND clang = 1');
-
-			foreach ($cats as $id => $data) {
-				// make update
-				$new_path = $to_path.$from_cat.'|'.str_replace($from_path, '', $data['path']);
-
-				// path Ã¤ndern und speichern
-				$up->setTable($prefix.'article');
-				$up->setWhere('id = '.$id);
-				$up->setValue('path', $new_path);
-				$up->update();
-				$up->flush();
-
-				// cat in gen eintragen
-				$RC[$id] = 1;
-			}
-
-			// clang holen, max catprio holen und entsprechend updaten
-
-			foreach (sly_Util_Language::findAll(true) as $clang) {
-				$catprior = (int) rex_sql::fetch('MAX(catprior)', 'article', 're_id = '.$to_cat.' AND clang = '.$clang);
-
-				$up->setTable($prefix.'article');
-				$up->setWhere('id = '.$from_cat.' AND clang = '.$clang);
-				$up->setValue('path', $to_path);
-				$up->setValue('re_id', $to_cat);
-				$up->setValue('catprior', $catprior + 1);
-				$up->update();
-				$up->flush();
-
-				// Cache aufräumen
-				$cache = sly_Core::cache();
-				$cache->delete('sly.category', $from_cat.'_'.$clang);
-				$cache->delete('sly.category.list', $to_cat.'_'.$clang.'_0');
-				$cache->delete('sly.category.list', $to_cat.'_'.$clang.'_1');
-			}
-
-			// generiere Artikel neu - ohne neue Inhaltsgenerierung
-			foreach ($RC as $id => $key) {
-				rex_deleteCacheArticle($id);
-			}
-
-			foreach (sly_Util_Language::findAll(true) as $clang) {
-				rex_newCatPrio($from_data['re_id'], $clang, 0, 1);
-			}
-		}
 	}
+
+	// folgende cats regenerate
+
+	$toDelete[$oldParent] = 1;
+	$toDelete[$from_cat]  = 1;
+	$toDelete[$to_cat]    = 1;
+
+	if ($to_cat > 0) {
+		$to_path  = $to_data['path'].$to_cat.'|';
+		$to_re_id = $to_data['re_id'];
+	}
+	else {
+		$to_path  = '|';
+		$to_re_id = 0;
+	}
+
+	// update paths
+
+	$from_path = $from_data['path'].$from_cat.'|';
+	$cats      = array();
+
+	$sql->query('SELECT id, path FROM '.$prefix.'article WHERE path LIKE "'.$from_path.'%" AND clang = 1');
+
+	foreach ($sql as $row) {
+		$cats[$row['id']] = $row['path'];
+	}
+
+	foreach ($cats as $id => $path) {
+		// update
+		$new_path = $to_path.$from_cat.'|'.str_replace($from_path, '', $path);
+		$sql->update('article', array('path' => $new_path), array('id' => $id));
+
+		// cat in gen eintragen
+		$toDelete[$id] = 1;
+	}
+
+	// clang holen, max catprio holen und entsprechend updaten
+
+	foreach (sly_Util_Language::findAll(true) as $clang) {
+		$catprior = (int) $sql->magicFetch('article', 'catprior', array('id' => $from_cat, 'clang' => $clang));
+		$max      = (int) $sql->magicFetch('article', 'MAX(catprior)', array('re_id' => $to_cat, 'clang' => $clang));
+
+		// update the category itself
+		$sql->update('article', array('path' => $to_path, 're_id' => $to_cat, 'catprior' => $max + 1), array('id' => $from_cat, 'clang' => $clang));
+
+		// move the remaining categories up
+		$sql->query('UPDATE '.$prefix.'article SET catprior = catprior - 1 WHERE re_id = '.$oldParent.' AND startpage = 1 AND clang = '.$clang.' AND catprior > '.$catprior);
+	}
+
+	// update cache (very generously since many categories have changed (-> been moved))
+	$cache->flush('sly.category', true);
+
+	// generiere Artikel neu - ohne neue Inhaltsgenerierung
+	foreach (array_keys($toDelete) as $id) rex_deleteCacheArticle($id);
 
 	return true;
 }
