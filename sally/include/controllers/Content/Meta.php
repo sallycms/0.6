2458515c994c5af2316bd6cfc92a67f419216733
Zozi <zozi@webvariants.de>
2011-05-19 00:09:32 +0200

    just save this chnages

--- sally/include/controllers/Content/Meta.php
+++ sally/include/controllers/Content/Meta.php
@@ -16,6 +16,7 @@ class sly_Controller_Content_Meta extends sly_Controller_Content {
 	}
 
 	protected function index() {
+		if(!$this->article) return;
 		$this->header();
 		$this->render('index.phtml');
 	}
@@ -42,18 +43,34 @@ class sly_Controller_Content_Meta extends sly_Controller_Content {
 			if (sly_post('article2startpage', 'boolean', false) && $this->canMorphToStartpage()) {
 				if (rex_article2startpage($this->article->getId())) {
 					$this->info = t('content_tostartarticle_ok');
+					$this->article = sly_Util_Article::findById($this->article->getId());
 				} else {
 					$this->warning = t('content_tostartarticle_failed');
 				}
 			}
 
+			//copy content to another language
+			if (sly_post('copycontent', 'boolean', false)) {
+				if ($this->canCopyContent()) {
+					$clang_a = sly_post('clang_a', 'rex-clang-id');
+					$clang_b = sly_post('clang_b', 'rex-clang-id');
+
+					if (rex_copyContent($article_id, $article_id, $clang_a, $clang_b)) {
+						$this->info = t('content_contentcopy');
+					} else {
+						$this->warning = t('content_errorcopy');
+					}
+				}
+			}
+
 			//move article to other category
-			if (sly_post('movearticle', 'boolean', false) && $this->canMoveArticle()) {
+			if (sly_post('movearticle', 'boolean', false)) {
 				$new_category_id = sly_post('category_id_new', 'rex-category-id');
 
-				if (sly_Util_Category::hasPermissionOnCategory(sly_Util_User::getCurrentUser(), $new_category_id)) {
+				if (sly_Util_Category::hasPermissionOnCategory(sly_Util_User::getCurrentUser(), $new_category_id) && $this->canMoveArticle()) {
 					if (rex_moveArticle($this->article->getId(), $new_category_id)) {
 						$this->info = t('content_articlemoved');
+						$this->article = sly_Util_Article::findById($this->article->getId());
 					} else {
 						$this->warning = t('content_errormovearticle');
 					}
@@ -62,7 +79,34 @@ class sly_Controller_Content_Meta extends sly_Controller_Content {
 				}
 			}
 			
-			$this->article = sly_Util_Article::findById($this->article->getId(), $this->article->getClang());
+			if (sly_post('copyarticle', 'boolean', false)) {
+				$new_category_id = sly_post('category_copy_id_new', 'rex-category-id');
+
+				if ($this->canCopyArticle()) {
+					if (($new_id = rex_copyArticle($this->article->getId(), $new_category_id)) !== false) {
+						$this->info = t('content_articlecopied');
+						$this->article = sly_Util_Article::findById($new_id);
+					} else {
+						$this->warning = t('content_errorcopyarticle');
+					}
+				} else {
+					$this->warning = t('no_rights_to_this_function');
+				}
+			}
+
+			if (sly_post('movecategory', 'string')) {
+				$new_category_id = sly_post('category_id_new', 'rex-category-id');
+				if (sly_Util_Category::hasPermissionOnCategory(sly_Util_User::getCurrentUser(), $new_category_id) && $this->canMoveCategory()) { 
+					if (rex_moveCategory($this->article->getCategoryId(), $new_category_id)) {
+						$this->info = t('category_moved');
+						$this->article = sly_Util_Article::findById($this->article->getCategoryId());
+					} else {
+						$this->warning = t('content_error_movecategory');
+					}
+				} else {
+					$this->warning = t('no_rights_to_this_function');
+				}
+			}
 		} catch (Exception $e) {
 			$this->warning = $e->getMessage();
 		}
@@ -83,5 +127,19 @@ class sly_Controller_Content_Meta extends sly_Controller_Content {
 		$user = sly_Util_User::getCurrentUser();
 		return $user->isAdmin() || $user->hasPerm('article2startpage[]');
 	}
+	
+	protected function canCopyContent() {
+		$user = sly_Util_User::getCurrentUser();
+		return $user->isAdmin() || $user->hasPerm('copyContent[]') && sly_Util_Language::isMultilingual();
+	}
+	
+	protected function canCopyArticle() {
+		$user = sly_Util_User::getCurrentUser();
+		return $user->isAdmin() || $user->hasPerm('copyArticle[]');
+	}
 
+	protected function canMoveCategory() {
+		$user = sly_Util_User::getCurrentUser();
+		return $this->article->isStartArticle() && $user->isAdmin() || $user->hasPerm('moveCategory[]');
+	}
 }
