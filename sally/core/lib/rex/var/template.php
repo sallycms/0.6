6e255ec0e2acf9b5908eb76375d6fee613b6be3f
Christoph <christoph@webvariants.de>
2011-07-22 00:02:17 +0200

    removed unused prefix/suffix/ifempty/instead args for REX_TEMPLATE, eliminating the use of eval()

--- sally/core/lib/rex/var/template.php
+++ sally/core/lib/rex/var/template.php
@@ -8,7 +8,7 @@
  */
 
 /**
- * REX_TEMPLATE[2]
+ * REX_TEMPLATE[mytemplate]
  *
  * @ingroup redaxo
  */
@@ -32,31 +32,14 @@ class rex_var_template extends rex_var {
 			list ($template_id, $args) = $this->extractArg('id', $args, 0);
 
 			if (!empty($template_id) && $tplService->exists($template_id)) {
-				$varname = '$__rex_tpl'.$template_id;
-				$tpl     = "<?php\n\$tplService = sly_Service_Factory::getTemplateService();";
+				$tpl = "<?php\n\$tplService = sly_Service_Factory::getTemplateService();";
 
 				if (isset($args['callback'])) {
 					$tpl .= "\n".'$args[\'subject\'] = file_get_contents($tplService->getContent(\'$template_id\'));';
 					$tpl .= "\n".'eval(\'?>\'.call_user_func(unserialize("'.serialize($args['callback']).'", $args));';
 				}
 				else {
-					$prefix = isset($args['prefix']) ? "\n".'eval("'.addslashes($args['prefix']).'")' : '';
-					$suffix = isset($args['suffix']) ? "\n".'eval("'.addslashes($args['suffix']).'")' : '';
-					$exists = $tplService->exists($template_id);
-
-					$tpl .= $prefix;
-
-					if (isset($args['instead']) && $exists) { // Bescheuertes Verhalten von REDAXO beibehalten.
-						$tpl .= "\n".'eval("'.addslashes($args['instead']).'");';
-					}
-					elseif (isset($args['ifempty']) && !$exists) {
-						$tpl .= "\n".'eval("'.addslashes($args['ifempty']).'");';
-					}
-					else {
-						$tpl .= "\n".'$tplService->includeFile(\''.$template_id.'\');';
-					}
-
-					$tpl .= $suffix;
+					$tpl .= "\n".'$tplService->includeFile(\''.$template_id.'\');';
 				}
 
 				$tpl .= "\n".'?>';
