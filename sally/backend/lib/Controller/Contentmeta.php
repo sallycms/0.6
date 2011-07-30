1dad2ce80fa95496de16eb1fd414260b67934396
Christoph <christoph@webvariants.de>
2011-07-30 16:18:30 +0200

    moved all actions into their own methods

--- sally/backend/lib/Controller/Contentmeta.php
+++ sally/backend/lib/Controller/Contentmeta.php
@@ -10,13 +10,13 @@
  */
 
 class sly_Controller_Contentmeta extends sly_Controller_Content_Base {
-
 	protected function index() {
-		if ($this->header() !== true)
-			return;
+		if ($this->header() !== true) return;
 
-		print $this->render('content/meta/index.phtml', array('article' => $this->article,
-					'user' => sly_Util_User::getCurrentUser()));
+		print $this->render('content/meta/index.phtml', array(
+			'article' => $this->article,
+			'user'    => sly_Util_User::getCurrentUser()
+		));
 	}
 
 	protected function getPageName() {
@@ -24,98 +24,136 @@ class sly_Controller_Contentmeta extends sly_Controller_Content_Base {
 	}
 
 	protected function processMetaForm() {
+		require_once SLY_COREFOLDER.'/functions/function_rex_content.inc.php';
 
-		require_once SLY_COREFOLDER . '/functions/function_rex_content.inc.php';
 		try {
-			//save metadata
+			// save metadata
 			if (sly_post('savemeta', 'boolean', false)) {
-				$name = sly_post('meta_article_name', 'string');
-
-				sly_Service_Factory::getArticleService()->edit($this->article->getId(), $this->article->getClang(), $name);
-
-				// notify system
-				$this->info = t('metadata_updated');
-				sly_Core::dispatcher()->notify('SLY_ART_META_UPDATED', null, array(
-					'id' => $this->article->getId(),
-					'clang' => $this->article->getClang()
-				));
-				$this->article = sly_Util_Article::findById($this->article->getId());
+				$this->saveMeta();
 			}
 
-			//make article the startarticle
+			// make article the startarticle
 			if (sly_post('article2startpage', 'boolean', false) && $this->canMorphToStartpage()) {
-				if (rex_article2startpage($this->article->getId())) {
-					$this->info = t('content_tostartarticle_ok');
-					$this->article = sly_Util_Article::findById($this->article->getId());
-				} else {
-					$this->warning = t('content_tostartarticle_failed');
-				}
+				$this->morphToStartpage();
 			}
 
-			//copy content to another language
+			// copy content to another language
 			if (sly_post('copycontent', 'boolean', false)) {
-				if ($this->canCopyContent()) {
-					$clang_a    = sly_post('clang_a', 'rex-clang-id');
-					$clang_b    = sly_post('clang_b', 'rex-clang-id');
-					$article_id = $this->article->getId();
-
-					if (rex_copyContent($article_id, $article_id, $clang_a, $clang_b)) {
-						$this->info = t('content_contentcopy');
-					} else {
-						$this->warning = t('content_errorcopy');
-					}
-				}
+				$this->copyContent();
 			}
 
-			//move article to other category
+			// move article to other category
 			if (sly_post('movearticle', 'boolean', false)) {
-				$new_category_id = sly_post('category_id_new', 'rex-category-id');
-
-				if (sly_Util_Category::hasPermissionOnCategory(sly_Util_User::getCurrentUser(), $new_category_id) && $this->canMoveArticle()) {
-					if (rex_moveArticle($this->article->getId(), $new_category_id)) {
-						$this->info = t('content_articlemoved');
-						$this->article = sly_Util_Article::findById($this->article->getId());
-					} else {
-						$this->warning = t('content_errormovearticle');
-					}
-				} else {
-					$this->warning = t('no_rights_to_this_function');
-				}
+				$this->moveArticle();
 			}
 
 			if (sly_post('copyarticle', 'boolean', false)) {
-				$new_category_id = sly_post('category_copy_id_new', 'rex-category-id');
-
-				if ($this->canCopyArticle()) {
-					if (($new_id = rex_copyArticle($this->article->getId(), $new_category_id)) !== false) {
-						$this->info = t('content_articlecopied');
-						$this->article = sly_Util_Article::findById($new_id);
-					} else {
-						$this->warning = t('content_errorcopyarticle');
-					}
-				} else {
-					$this->warning = t('no_rights_to_this_function');
-				}
+				$this->copyArticle();
 			}
 
 			if (sly_post('movecategory', 'string')) {
-				$new_category_id = sly_post('category_id_new', 'rex-category-id');
-				if (sly_Util_Category::hasPermissionOnCategory(sly_Util_User::getCurrentUser(), $new_category_id) && $this->canMoveCategory()) {
-					if (rex_moveCategory($this->article->getCategoryId(), $new_category_id)) {
-						$this->info = t('category_moved');
-						$this->article = sly_Util_Article::findById($this->article->getCategoryId());
-					} else {
-						$this->warning = t('content_error_movecategory');
-					}
-				} else {
-					$this->warning = t('no_rights_to_this_function');
-				}
+				$this->moveCategory();
 			}
-		} catch (Exception $e) {
+		}
+		catch (Exception $e) {
 			$this->warning = $e->getMessage();
 		}
 
 		$this->index();
+	}
+
+	private function saveMeta() {
+		$name = sly_post('meta_article_name', 'string');
+
+		sly_Service_Factory::getArticleService()->edit($this->article->getId(), $this->article->getClang(), $name);
+
+		// notify system
+		$this->info = t('metadata_updated');
+
+		sly_Core::dispatcher()->notify('SLY_ART_META_UPDATED', null, array(
+			'id'    => $this->article->getId(),
+			'clang' => $this->article->getClang()
+		));
+
+		$this->article = sly_Util_Article::findById($this->article->getId());
+	}
+
+	private function morphToStartpage() {
+		if (rex_article2startpage($this->article->getId())) {
+			$this->info = t('content_tostartarticle_ok');
+			$this->article = sly_Util_Article::findById($this->article->getId());
+		}
+		else {
+			$this->warning = t('content_tostartarticle_failed');
+		}
+	}
+
+	private function copyContent() {
+		if ($this->canCopyContent()) {
+			$clang_a    = sly_post('clang_a', 'rex-clang-id');
+			$clang_b    = sly_post('clang_b', 'rex-clang-id');
+			$article_id = $this->article->getId();
+
+			if (rex_copyContent($article_id, $article_id, $clang_a, $clang_b)) {
+				$this->info = t('content_contentcopy');
+			}
+			else {
+				$this->warning = t('content_errorcopy');
+			}
+		}
+	}
+
+	private function moveArticle() {
+		$target = sly_post('category_id_new', 'rex-category-id');
+
+		if (sly_Util_Category::hasPermissionOnCategory(sly_Util_User::getCurrentUser(), $target) && $this->canMoveArticle()) {
+			if (rex_moveArticle($this->article->getId(), $target)) {
+				$this->info    = t('content_articlemoved');
+				$this->article = sly_Util_Article::findById($this->article->getId());
+			}
+			else {
+				$this->warning = t('content_errormovearticle');
+			}
+		}
+		else {
+			$this->warning = t('no_rights_to_this_function');
+		}
+	}
+
+	private function copyArticle() {
+		$target = sly_post('category_copy_id_new', 'rex-category-id');
+
+		if ($this->canCopyArticle()) {
+			$new_id = rex_copyArticle($this->article->getId(), $target);
+
+			if ($new_id !== false) {
+				$this->info    = t('content_articlecopied');
+				$this->article = sly_Util_Article::findById($new_id);
+			}
+			else {
+				$this->warning = t('content_errorcopyarticle');
+			}
+		}
+		else {
+			$this->warning = t('no_rights_to_this_function');
+		}
+	}
+
+	private function moveCategory() {
+		$target = sly_post('category_id_new', 'rex-category-id');
+
+		if (sly_Util_Category::hasPermissionOnCategory(sly_Util_User::getCurrentUser(), $target) && $this->canMoveCategory()) {
+			if (rex_moveCategory($this->article->getCategoryId(), $target)) {
+				$this->info    = t('category_moved');
+				$this->article = sly_Util_Article::findById($this->article->getCategoryId());
+			}
+			else {
+				$this->warning = t('content_error_movecategory');
+			}
+		}
+		else {
+			$this->warning = t('no_rights_to_this_function');
+		}
 	}
 
 	/**
@@ -146,5 +184,4 @@ class sly_Controller_Contentmeta extends sly_Controller_Content_Base {
 		$user = sly_Util_User::getCurrentUser();
 		return $this->article->isStartArticle() && $user->isAdmin() || $user->hasRight('moveCategory[]');
 	}
-
 }
