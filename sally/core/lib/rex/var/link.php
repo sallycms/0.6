e6e6f0403b20705ba36f3f0f529d53fe1e1eff89
Zozi <zozi@webvariants.de>
2011-08-02 13:51:52 +0200

    fix a non fix, we need to discuss this

--- sally/core/lib/rex/var/link.php
+++ sally/core/lib/rex/var/link.php
@@ -213,7 +213,7 @@ class rex_var_link extends rex_var {
 	 */
 	public function getLinkWidget($id, $article_id, $category = '') {
 		// TODO: Build something like $widget->setRootCat($category);
-		$widget = new sly_Form_Widget_Link(self::LINK.'['.$id.']', null, $article_id, $id);
+		$widget = new sly_Form_Widget_Link('LINK['.$id.']', null, $article_id, $id);
 
 		return $widget->render();
 	}
@@ -224,7 +224,7 @@ class rex_var_link extends rex_var {
 	public function getLinklistWidget($id, $value, $category = '') {
 		// TODO: Build something like $widget->setRootCat($category);
 		$articles = explode(',', $value);
-		$widget   = new sly_Form_Widget_LinkList(self::LINKLIST.'['.$id.']', null, $articles, $id);
+		$widget   = new sly_Form_Widget_LinkList('LINKLIST['.$id.']', null, $articles, $id);
 		
 		return $widget->render();
 	}
