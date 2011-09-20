56f5384066b6fa3a6e4342a987b3ad5db7e31c65
Zozi <zozi@webvariants.de>
2011-09-20 11:51:52 +0200

    do neot let the layout get the lang automatically, this was bad design

--- sally/core/lib/sly/Layout/XHTML5.php
+++ sally/core/lib/sly/Layout/XHTML5.php
@@ -13,12 +13,21 @@
  */
 class sly_Layout_XHTML5 extends sly_Layout_XHTML {
 	protected $manifest; ///< string
+	protected $language; ///< string
 
 	/**
 	 * @param string $manifest
 	 */
 	public function setManifest($manifest) {
 		$this->manifest = $manifest;
+	}
+	
+	/**
+	 * the short locale (de,en,fr...)
+	 * @param string $language 
+	 */
+	public function setLanguage($language) {
+		$this->language = $language;
 	}
 
 	/**
