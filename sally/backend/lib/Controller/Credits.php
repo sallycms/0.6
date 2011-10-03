cf54db1088cda06e86a103d50629620444147c05
Christoph <christoph@webvariants.de>
2011-10-03 05:11:09 +0200

    improved version detection on bugreport page

--- sally/backend/lib/Controller/Credits.php
+++ sally/backend/lib/Controller/Credits.php
@@ -16,8 +16,8 @@ class sly_Controller_Credits extends sly_Controller_Backend {
 
 		if (sly_Util_User::getCurrentUser()->isAdmin()) {
 			$subline = array(
-				array('', t('credits')),
-				array('bugreport', 'Fehler gefunden?')
+				array('credits', t('credits')),
+				array('credits_bugreport', 'Fehler gefunden?')
 			);
 		}
 
--- sally/backend/lib/Controller/Credits/Bugreport.php
+++ sally/backend/lib/Controller/Credits/Bugreport.php
@@ -23,12 +23,28 @@ class sly_Controller_Credits_Bugreport extends sly_Controller_Credits {
 		return $langs;
 	}
 
-	protected function getMySQLVersion() {
-		$db = sly_DB_Persistence::getInstance();
-		$db->query('SELECT VERSION()');
-		foreach ($db as $row) $version = reset($row);
+	protected function getDatabaseVersion() {
+		$driver = strtolower(sly_Core::config()->get('DATABASE/DRIVER'));
 
-		return $version;
+		switch ($driver) {
+			case 'mysql':
+			case 'pgsql':
+				$db = sly_DB_Persistence::getInstance();
+				$db->query('SELECT VERSION()');
+				foreach ($db as $row) $version = reset($row);
+				break;
+
+			case 'sqlite':
+				$version = SQLite3::version();
+				$version = $version['versionString'];
+				break;
+
+			case 'oci':
+			default:
+				$version = 'N/A';
+		}
+
+		return compact('driver', 'version');
 	}
 
 	protected function getExtensions() {
