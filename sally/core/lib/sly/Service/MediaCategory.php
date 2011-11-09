aa6625d6112bddb960cbc4d93a682307ce3694c2
Christoph <christoph@webvariants.de>
2011-11-10 00:31:14 +0100

    fixed adding media categories

--- sally/core/lib/sly/Service/MediaCategory.php
+++ sally/core/lib/sly/Service/MediaCategory.php
@@ -119,6 +119,7 @@ class sly_Service_MediaCategory extends sly_Service_Model_Base_Id {
 		$category = new sly_Model_MediaCategory();
 		$category->setName($title);
 		$category->setRevision(0);
+		$category->setAttributes('');
 		$category->setCreateColumns();
 
 		$this->setPath($category, $parent);
