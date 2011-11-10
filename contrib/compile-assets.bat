96c8a537e43ddf1164333cb947d120005c2a7773
Christoph <christoph@webvariants.de>
2011-11-10 18:05:29 +0100

    let's use UglifyJS from here on

--- contrib/compile-assets.bat
+++ contrib/compile-assets.bat
@@ -1,3 +1,4 @@
-java -jar Q:\alfred\yuicompressor.jar -v --charset utf8 --type js -o ../sally/backend/assets/js/standard.min.js ../sally/backend/assets/js/standard.js
-java -jar Q:\alfred\yuicompressor.jar -v --charset utf8 --type js -o ../sally/backend/assets/js/jquery.timepicker.min.js ../sally/backend/assets/js/jquery.timepicker.js
-java -jar Q:\alfred\yuicompressor.jar -v --charset utf8 --type js -o ../sally/backend/assets/js/jquery.imgcheckbox.min.js ../sally/backend/assets/js/jquery.imgcheckbox.js
+@echo off
+call uglifyjs -nc -o ../sally/backend/assets/js/standard.min.js ../sally/backend/assets/js/standard.js
+call uglifyjs -nc -o ../sally/backend/assets/js/jquery.timepicker.min.js ../sally/backend/assets/js/jquery.timepicker.js
+call uglifyjs -nc -o ../sally/backend/assets/js/jquery.imgcheckbox.min.js ../sally/backend/assets/js/jquery.imgcheckbox.js
--- contrib/compile-assets.sh
+++ contrib/compile-assets.sh
@@ -1,4 +1,4 @@
 #/bin/sh
-java -jar ../../alfred/yuicompressor.jar -v --charset utf8 --type js -o ../sally/backend/assets/js/standard.min.js ../sally/backend/assets/js/standard.js
-java -jar ../../alfred/yuicompressor.jar -v --charset utf8 --type js -o ../sally/backend/assets/js/jquery.timepicker.min.js ../sally/backend/assets/js/jquery.timepicker.js
-java -jar ../../alfred/yuicompressor.jar -v --charset utf8 --type js -o ../sally/backend/assets/js/jquery.imgcheckbox.min.js ../sally/backend/assets/js/jquery.imgcheckbox.js
+uglifyjs -nc -o ../sally/backend/assets/js/standard.min.js ../sally/backend/assets/js/standard.js
+uglifyjs -nc -o ../sally/backend/assets/js/jquery.timepicker.min.js ../sally/backend/assets/js/jquery.timepicker.js
+uglifyjs -nc -o ../sally/backend/assets/js/jquery.imgcheckbox.min.js ../sally/backend/assets/js/jquery.imgcheckbox.js
