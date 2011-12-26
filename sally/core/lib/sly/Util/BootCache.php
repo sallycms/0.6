71227e05d4a47c1d20e2ae75856939473c20fe39
Christoph <christoph@webvariants.de>
2011-12-26 02:20:52 +0100

    fixed bootcache

--- sally/core/lib/sly/Util/BootCache.php
+++ sally/core/lib/sly/Util/BootCache.php
@@ -113,8 +113,7 @@ class sly_Util_BootCache {
 			'lib/compatibility.php',
 			'lib/functions.php',
 			'functions/function_rex_globals.inc.php',
-			'functions/function_rex_client_cache.inc.php',
-			'functions/function_rex_other.inc.php'
+			'functions/function_rex_client_cache.inc.php'
 		);
 
 		foreach ($functionFiles as $fctFile) {
